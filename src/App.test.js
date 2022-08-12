03741a9c4754f9becdb75e683e792b15160b24d2
AbhijitSarkaar <theabhijitsarkar@gmail.com>
2022-08-12 09:59:24 +0530

    test setup and first test

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('render text', () => {
-  render(<App />);
-  const element = screen.getByText('Hi there!');
-  expect(element).toBeInTheDocument();
-});
