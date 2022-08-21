f48332aa11e78fe99a32ff284021785c0839263d
AbhijitSarkaar <theabhijitsarkar@gmail.com>
2022-08-21 13:02:16 +0530

    change react version to 16

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -5,8 +5,7 @@ import CommentBox from "components/CommentBox";
 it('has a textarea and a button', () => {
     const wrapped = mount(<CommentBox />)
 
-    console.log(wrapped.find('textarea').length)
-    console.log(wrapped.find('button').length)
+   
 
 
 })
